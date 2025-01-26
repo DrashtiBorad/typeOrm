8aba86247b5596af807314974800fad1b26dec42
DrashtiBorad <djborad0890@gmail.com>
2025-01-26 22:40:23 +0530

    add configuration filr.

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,16 @@
 import express from "express";
-import { appDataSource } from "./config/database";
-import { router } from "./routes";
 import dotenv from "dotenv";
 import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(router);
-dotenv.config();
 
 const port = 3003;
+
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server started successfully." });
 });
 
-appDataSource
-  .initialize()
-  .then(() => {
-    console.log("connected");
-    app.listen(port, () => {
-      console.log(`server starting at port ${port}`);
-    });
-  })
-  .catch((err) => console.log("err", err));
+app.listen(port, () => {
+  console.log(`server started on port: ${port}`);
+});
